a26d6199144a83ea6aaca50ea8dc558f6501adca
Maria Fernanda Ramirez Serna <maferamirezs2000@gmail.com>
2024-11-06 17:55:18 -0500

    update jug

--- jugadores.py
+++ jugadores.py
@@ -31,6 +31,11 @@ def registrar_jugador(id_jugador, nombre, numero, posicion, equipo_id):
     Retorno de datos:
         list: Devuelve la lista de jugadores actualizada, o None si ya existe un jugador con el mismo ID.
     """
+
+    if not any(equipo["id"] == equipo_id for equipo in equipos):
+        print(f"Error: El equipo con ID {equipo_id} no existe. No se puede registrar el jugador.")
+        return None
+
     if jugador_existe(id_jugador):
         print(f"Error: Ya existe un jugador con el ID {id_jugador}.")
         return None
